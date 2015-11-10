9c0d099d65c1336225a1b4e27047314b467a0473
Marco Cipriani <marco.cipriani@roma1.infn.it>
2015-11-10 12:02:29 +0100

    added zlljets_metResoResp

--- include/EmanTreeAnalysis.h
+++ include/EmanTreeAnalysis.h
@@ -10,102 +10,10 @@
 #include <cstdlib>
 #include <vector>
 
-#include "edimarcoTree.h"
-//#include "edimarcoTreeFriend.h"
-#include "edimarcoTree_noSkim.h"
 #include "edimarcoTree_v2.h"
 
 namespace myAnalyzerTEman {
 
-  class znunujetsAna : public edimarcoTree_v2 /*,public edimarcoTreeFriend*/ {
-  public:
-
-    znunujetsAna(TTree *tree);
-    virtual ~znunujetsAna() { std::cout<<"~znunujetsAna() called"<<std::endl; }
-  
-    void loop(const char* configFileName);
-
-  };
-
-  class zmumujetsAna : public edimarcoTree /*,public edimarcoTreeFriend*/ {
-  public:
-
-    zmumujetsAna(TTree *tree);
-    virtual ~zmumujetsAna() { std::cout<<"~zmumujetsAna() called"<<std::endl; }
-  
-    void loop();
-
-  };
-
-  class zmumujetsAna_LepSk : public edimarcoTree /*,public edimarcoTreeFriend*/ {
-  public:
-
-    zmumujetsAna_LepSk(TTree *tree);
-    virtual ~zmumujetsAna_LepSk() { std::cout<<"~zmumujetsAna_LepSk() called"<<std::endl; }
-  
-    void loop();
-
-  };
-
-  class zeejetsAna : public edimarcoTree /*,public edimarcoTreeFriend*/ {
-  public:
-
-    zeejetsAna(TTree *tree);
-    virtual ~zeejetsAna() { std::cout<<"~zeejetsAna() called"<<std::endl; }
-  
-    void loop();
-
-  };
-
-  class zmumujetsControlSample : public edimarcoTree /*,public edimarcoTreeFriend*/ {
-  public:
-
-    zmumujetsControlSample(TTree *tree, const char*);
-    virtual ~zmumujetsControlSample() { std::cout<<"~zmumujetsControlSample() called"<<std::endl; }
-  
-    void loop(std::vector< Double_t > &, std::vector< Double_t > &);
-    std::string suffix;
-  };
-
-  class zeejetsControlSample : public edimarcoTree /*,public edimarcoTreeFriend*/ {
-  public:
-
-    zeejetsControlSample(TTree *tree, const char*);
-    virtual ~zeejetsControlSample() { std::cout<<"~zeejetsControlSample() called"<<std::endl; }
-  
-    void loop(std::vector< Double_t > &, std::vector< Double_t > &);
-    std::string suffix;
-  };
-  
-  class zlljetsAna : public edimarcoTree /*,public edimarcoTreeFriend*/ {
-  public:
-
-    zlljetsAna(TTree *tree);
-    virtual ~zlljetsAna() { std::cout<<"~zlljetsAna() called"<<std::endl; }
-  
-    void loop();
-
-  };
-
-  class zlljetsAna_new : public edimarcoTree_v2 /*,public edimarcoTreeFriend*/ {
-  public:
-
-    zlljetsAna_new(TTree *tree);
-    virtual ~zlljetsAna_new() { std::cout<<"~zlljetsAna_new() called"<<std::endl; }
-  
-    void loop(const char* configFileName, const Int_t ISDATA_FLAG);
-
-  };
-
-  class zlljets_Axe_noSkim : public edimarcoTree_v2 /*,public edimarcoTreeFriend*/ {
-  public:
-
-    zlljets_Axe_noSkim(TTree *tree);
-    virtual ~zlljets_Axe_noSkim() { std::cout<<"~zlljets_Axe_noSkim() called"<<std::endl; }
-  
-    void loop(const char* configFileName);
-
-  };
 
   class zlljets_Axe_noSkim_light : public edimarcoTree_v2 /*,public edimarcoTreeFriend*/ {
   public:
@@ -125,9 +33,41 @@ namespace myAnalyzerTEman {
     zlljets_resoResp(TTree *tree);
     virtual ~zlljets_resoResp() { std::cout<<"~zlljets_resoResp() called"<<std::endl; }
   
-    void loop(const char* configFileName, const Int_t ISDATA_FLAG, const Int_t unweighted_event_flag = 0);
+    void loop(const char* configFileName, const Int_t ISDATA_FLAG, const Int_t unweighted_event_flag);
 
   };
+
+  class zlljetsControlSample : public edimarcoTree_v2 {
+  public:
+
+    zlljetsControlSample(TTree *tree, const char*);  // the char* is the sample's name (e.g. QCD, Znunu, ecc...)
+    virtual ~zlljetsControlSample() { std::cout<<"~zlljetsControlSample() called"<<std::endl; }
+  
+    void loop(const char* configFileName, const Int_t ISDATA_FLAG, const Int_t unweighted_event_flag, std::vector< Double_t > &, std::vector< Double_t > &, std::vector< Double_t > &);
+    std::string suffix;
+  };
+
+  class monojet_SignalRegion : public edimarcoTree_v2 {
+  public:
+
+    monojet_SignalRegion(TTree *tree, const char*);  // the char* is the sample's name (e.g. QCD, Znunu, ecc...)
+    virtual ~monojet_SignalRegion() { std::cout<<"~monojet_SignalRegion() called"<<std::endl; }
+  
+    void loop(const char* configFileName, const Int_t ISDATA_FLAG, const Int_t unweighted_event_flag, std::vector< Double_t > &, std::vector< Double_t > &, std::vector< Double_t > &);
+    std::string suffix;
+  };
+
+  class zlljets_metResoResp : public edimarcoTree_v2 {
+  public:
+
+    zlljets_metResoResp(TTree *tree, const char*);  // the char* is the sample's name (e.g. QCD, Znunu, ecc...)
+    virtual ~zlljets_metResoResp() { std::cout<<"~zlljets_metResoResp() called"<<std::endl; }
+  
+    void loop(const char* configFileName, const Int_t ISDATA_FLAG, const Int_t unweighted_event_flag, std::vector< Double_t > &, std::vector< Double_t > &, std::vector< Double_t > &);
+    std::string suffix;
+  };
+
+
 
 }
 #endif
